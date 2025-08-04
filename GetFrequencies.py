71f686d6fb60d2526d7e7e447c9c146c68a0af2b
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-08-03 17:26:49 -0700

    Attempt to detect note types properly
    Properly converts to tab, currently trying to smoothen out tablature notes (detecting whole, half, quarter, eighth notes) properly

--- GetFrequencies.py
+++ GetFrequencies.py
@@ -1,9 +1,11 @@
 import crepe
 import librosa
 import pandas as pd
-from music21 import stream, note, pitch
+from music21 import stream, note, pitch, tablature
+
 
 standard_bass = ['E1', 'A1', 'D2', 'G2']
+
 
 def stable_note_grouping(times, freqs, confs, threshold=0.7, stability=3):
     data = [
@@ -16,7 +18,6 @@ def stable_note_grouping(times, freqs, confs, threshold=0.7, stability=3):
     cur_start = None
     candidate_note = None
     count = 0
-
     for _, (t, _, note_str) in enumerate(data):
         if note_str == cur_note:
             candidate_note = None
@@ -42,6 +43,7 @@ def stable_note_grouping(times, freqs, confs, threshold=0.7, stability=3):
 
     return groups
 
+
 def note_to_string_fret(note_str):
     midi = pitch.Pitch(note_str).midi
     for string_num, string_note in enumerate(standard_bass):
@@ -49,11 +51,19 @@ def note_to_string_fret(note_str):
         fret = midi - string_midi
         if 0 <= fret <= 24:
             return (string_num + 1, fret) 
-    return None  # not playable on standard tuning
+    return None  # not playable on bass standard tuning
 
-def create_musicxml_from_notes(note_data):
+
+def create_musicxml_from_notes(note_data, BPM):
+    
+    def seconds_to_quarter_length(start, end, bpm=BPM):
+      duration_sec = end - start
+      beats = duration_sec * (bpm / 60)
+      note_lengths = [4, 2, 1, 0.5, 0.25]
+      return min(note_lengths, key=lambda x: abs(x - beats))
+    
     s = stream.Part()
-    s.id = 'Tab'
+    s.id = 'Bass'
 
     for entry in note_data:
         string_fret = note_to_string_fret(entry['note'])
@@ -61,14 +71,17 @@ def create_musicxml_from_notes(note_data):
             continue
 
         n = note.Note(entry['note'])
-        n.quarterLength = 1 
-        n.addLyric(f"String {string_fret[0]}, Fret {string_fret[1]}")
+        n.quarterLength = seconds_to_quarter_length(entry['start'], entry['end'])
+        # n.addLyric(f"String {string_fret[0]}, Fret {string_fret[1]}")
         s.append(n)
 
     s.write('musicxml', fp='bass_tab.xml')
     print("MusicXML file saved as 'bass_tab.xml'")
 
+
+
 waveform, sample_rate = librosa.load("bass.wav", sr=16000)
+tempo, _ = librosa.beat.beat_track(y=waveform, sr=sample_rate)
 time, frequency, confidence, _ = crepe.predict(waveform, sample_rate, viterbi=True)
 note_groups = stable_note_grouping(time, frequency, confidence)
 
@@ -80,4 +93,4 @@ note_data = [
 for entry in note_data:
     print(f"Start: {entry['start']:>5}s | End: {entry['end']:>5}s | Note: {entry['note']}")
 
-create_musicxml_from_notes(note_data)
+create_musicxml_from_notes(note_data, tempo)
