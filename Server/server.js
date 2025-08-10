65e83569b6cf85ec6923f7aabce11d53055bb1da
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-08-09 22:51:46 -0700

    Boiler Plate code removed, base server and frontend

--- Server/server.js
+++ Server/server.js
@@ -6,7 +6,9 @@ const PORT = 8080
 app.use(cors());
 
 app.get("/api/home", (req, res) => {
-  res.json({message: "Hello Twin."});
+  res.json({ 
+    message: "Hello Twin."
+  });
 });
 
 app.listen(PORT, () => {
