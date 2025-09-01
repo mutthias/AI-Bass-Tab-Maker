eb36cfcb8107a5421e81e5fba590f15a1b659cff
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-09-01 15:03:16 -0700

    User Login and Registration logic

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import pool from "../db.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import pool from "../db.js";
 
 const router = Router();
+const jwt_secret = process.env.JWT_SECRET;
 
-router.post("/users", async (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password, email } = req.body;
   try {
+    const hash = await bcrypt.hash(password, 10);
     const result = await pool.query(
       "INSERT INTO Users (username, password_hash, email) VALUES ($1, $2, $3) RETURNING *",
       [username, password, email]
@@ -15,5 +19,42 @@ router.post("/users", async (req, res) => {
     res.status(500).json({error: err.message});
   }
 });
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const res = await pool.query(
+      "SELECT * FROM Users WHERE email = $1", 
+      [email]
+    );
+
+    if (!user){
+      return res.status(401).json({error: "This user does not exist!"});
+    }
+
+    const valid = bcrypt.compare(password, user.password_hash);
+    if (!valid) {
+      return res.status(403).json({error: "Invalid password!"});
+    }
+
+    const token = jwt.sign(
+      { 
+        id: user.id, 
+        email: user.email
+      },
+        JWT_SECRET,
+      {
+        expiresIn: "1h"
+      }
+    );
+    res.status(200).then(res.json({token}));
+
+  } catch (err) {
+    res.status(500).json({
+      error: "Oops! Something went wrong."
+    });
+  }
+})
 
 export default router
