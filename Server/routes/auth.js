72833d154d65b8499e2eb5592e1f39f23600b8f2
Mathias Gabel <matthiasj.gabel@gmail.com>
2025-09-15 19:53:40 -0700

    Cookie functionality to middleware

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -3,15 +3,14 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export function authToken(req, res, next) {
-  const header = req.headers["authorization"];
-  const token = header && header.split(" ")[1]; // because header = "Bearer <token>"
-
+  const token = req.cookies.auth_token;
+  
   if (!token) {
     return res.status(401).json({
       error: "You are not authorized to access this application."
     });
   }
-
+  
   jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({
